aefa030b876650a94c7751303abe3423b1a2d2ec
Abhishek Anand <89356711+22abhirajput@users.noreply.github.com>
2022-10-05 23:14:57 +0530

    Time: 43 ms (22.55%), Space: 24.8 MB (86.98%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -12,14 +12,28 @@
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int v, int d) {
-        if (d == 0 || d == 1) {
+        if (d == 1) {
             TreeNode* newroot = new TreeNode(v);
-            (d ? newroot->left : newroot->right) = root;
+            newroot->left = root;
             return newroot;
         }
-        if (root && d >= 2) {
-            root->left  = addOneRow(root->left,  v, d > 2 ? d - 1 : 1);
-            root->right = addOneRow(root->right, v, d > 2 ? d - 1 : 0);
+        else if (d == 0) {
+            TreeNode* newroot = new TreeNode(v);
+            newroot->right = root;
+            return newroot;
+        }
+
+        if (!root) {
+            return nullptr;
+        }
+        else if (d == 2) {
+            root->left  = addOneRow(root->left,  v, 1);
+            root->right = addOneRow(root->right, v, 0);
+            return root;
+        }
+        else if (d > 2) {
+            root->left  = addOneRow(root->left,  v, d - 1);
+            root->right = addOneRow(root->right, v, d - 1);
         }
         return root;
     }
