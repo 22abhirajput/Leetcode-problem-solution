50580137c6428c00f8bfe75245a6b67c5358034c
Abhishek Anand <89356711+22abhirajput@users.noreply.github.com>
2022-11-21 00:18:21 +0530

    Time: 20 ms (61.02%), Space: 8 MB (72.15%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  int calculate(string s) {
+    int total = 0;
+    vector<int> signs(2, 1);
+    for (int i=0; i<s.size(); i++) {
+        char c = s[i];
+        if (c >= '0') {
+            long long number = 0;
+            while (i < s.size()  &&  s[i] >= '0')
+                number = 10 * number + s[i++] - '0';
+            total += signs.back() * number;
+            signs.pop_back();
+            i--;
+        }
+        else if(c=='(' and s[i+1]=='-'){}
+        else if (c == ')')
+            signs.pop_back();
+        else if (c != ' ')
+            signs.push_back(signs.back() * (c == '-' ? -1 : 1));
+    }
+    return total;
+}
+};
