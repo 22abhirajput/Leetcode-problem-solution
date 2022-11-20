656b9e7f455e2bd01cf49e0d529c01ad2af0d262
Abhishek Anand <89356711+22abhirajput@users.noreply.github.com>
2022-11-21 00:12:29 +0530

    Time: 423 ms (5.47%), Space: 43.6 MB (10.16%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -1,32 +1,53 @@
-class Solution 
-{
+// it's an Andrew's monotone chain convex hull algorithm that constructs the convex hull of a set of 2-dimensional points in O(nlogn) time.
+class Solution {
 public:
+    int getRotationAngle(vector<int> A, vector<int> B, vector<int> C) {
+        // (x2-x1)(y3-y1) - (y2-y1)(x3-x1) -> 3D Cross-product of AB and AC vectors
+        return ((B[0] - A[0]) * (C[1] - A[1])) - ((B[1] - A[1]) * (C[0] - A[0]));
+    }
     
-    using tree = vector<int>;
     
-    vector<tree> outerTrees(vector<tree>& trees) 
-    {
-        auto cross = [](tree& B, tree& A, tree& T) -> int
-        {
-            return (T[1]-B[1])*(B[0]-A[0]) - (B[1]-A[1])*(T[0]-B[0]);
-        };
-        
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        if (trees.size() <= 3) return trees;
         sort(trees.begin(), trees.end());
         
-        deque<tree> F;
-        
-        for (tree T : trees)
-        {
-            while (F.size() >= 2 and cross(F[F.size()-1],F[F.size()-2],T) < 0)
-                F.pop_back();
-            F.push_back(T);
-
-            while (F.size() >= 2 and cross(F[0],F[1],T) > 0)
-                F.pop_front();
-            F.push_front(T);
+		// Upper HULL construction
+        vector<vector<int>> lTrees;
+        lTrees.push_back(trees[0]);
+        lTrees.push_back(trees[1]);
+        for (int i = 2; i < trees.size(); i++) {
+            int ls = lTrees.size();
+            while (lTrees.size() >= 2 && getRotationAngle(lTrees[ls-2], lTrees[ls-1], trees[i]) > 0) {
+                lTrees.pop_back();
+                ls--;
+            }
+            lTrees.push_back(trees[i]);
         }
         
-        set<tree> unique(F.begin(), F.end());
-        return vector<tree>(unique.begin(), unique.end());
+		// Lower HULL construction
+        vector<vector<int>> rTrees;
+        rTrees.push_back(trees[trees.size() - 1]);
+        rTrees.push_back(trees[trees.size() - 2]);
+        for (int i = trees.size() - 3; i >= 0; --i) {
+            int rs = rTrees.size(); 
+            while (rTrees.size() >= 2 && getRotationAngle(rTrees[rs-2], rTrees[rs-1], trees[i]) > 0) {
+                rTrees.pop_back();
+                rs--;
+            }
+            rTrees.push_back(trees[i]);
+        }
+        
+		// Pop the last elements as they would've occurred in both lists
+        rTrees.pop_back();
+        lTrees.pop_back();
+        for (int i = 0; i < rTrees.size(); i++) { // concatinting lower hull to upper hull
+            lTrees.push_back(rTrees[i]);
+        }
+        
+		// Sort and remove duplicate elements. (It is begging for a different approach!!)
+        sort(lTrees.begin(), lTrees.end());
+        lTrees.erase(std::unique(lTrees.begin(), lTrees.end()), lTrees.end());
+        
+        return lTrees;
     }
 };
