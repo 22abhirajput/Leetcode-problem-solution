903213a39a4874a3cb4f582d16fb9744073c4daa
Abhishek Anand <89356711+22abhirajput@users.noreply.github.com>
2023-03-28 23:21:24 +0530

    Time: 7 ms (35.49%), Space: 10 MB (19.98%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+  unordered_set<int> travel(begin(days), end(days));
+  int dp[366] = {};
+  for (int i = 1; i < 366; ++i) {
+    if (travel.find(i) == travel.end()) dp[i] = dp[i - 1];
+    else dp[i] = min({ dp[i - 1] + costs[0], dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]});
+  }
+  return dp[365];
+}
+};
